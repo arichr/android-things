caa21b6bb55d12a7af2be82d8a177f750335f11a
Arisu W <arisuchr@riseup.net>
2022-07-19 15:46:34 +0300

    genlpmake.py: Fix partitions' size

--- genlpmake.py
+++ genlpmake.py
@@ -3,6 +3,7 @@
 import sys
 from pathlib import Path
 
+SPARSE_IMAGE_HEADER = (0x3A, 0xFF, 0x26, 0xED)
 AB_SUPER_PARTITIONS = {
     'system_a',
     'system_b',
@@ -15,6 +16,94 @@ AB_SUPER_PARTITIONS = {
     'system_ext_a',
     'system_ext_b',
 }
+
+
+def ask_sparse_image(raw_image_path: Path) -> (Path, Path):
+    """Ask for a sparse image.
+
+    Args:
+        raw_image_path: Raw image Path
+
+    Returns:
+        (Path, Path): The raw image's Path and the sparse image's Path
+    """
+    print(
+        f'[!] {raw_image_path.name} is an raw image.',
+        'To proceed, create an Android sparse image:',
+        f'img2simg {raw_image_path} {raw_image_path}.sparse.img\n',
+    )
+    while True:
+        sparse_image_path = Path(input('> '))
+        if sparse_image_path.is_file():
+            file_header = tuple(sparse_image_path.read_bytes()[:4])
+            if file_header == SPARSE_IMAGE_HEADER:
+                break
+
+            print(
+                f'[!] {sparse_image_path.name} is',
+                'NOT an Android sparse image.',
+            )
+        else:
+            print('[!] Wrong file location.')
+
+    if raw_image_path == sparse_image_path:
+        print(
+            '[!] It seems, you overwritten the raw image.',
+            "Please, DON'T do it again.",
+        )
+        return ask_raw_image(sparse_image_path), sparse_image_path
+
+    return raw_image_path, sparse_image_path
+
+
+def ask_raw_image(sparse_image_path: Path) -> (Path, Path):
+    """Ask for a raw image.
+
+    Args:
+        sparse_image_path: Android sparse image Path
+
+    Returns:
+        (Path, Path): The raw image's Path and the sparse image's Path
+    """
+    print(
+        f'[!] {sparse_image_path.name} is an Android sparse image.',
+        'To proceed, extract a raw image:',
+        f'simg2img {sparse_image_path} {sparse_image_path}.raw.img\n',
+    )
+    while True:
+        raw_image_path = Path(input('> '))
+        if raw_image_path.is_file():
+            if tuple(raw_image_path.read_bytes()[:4]) == SPARSE_IMAGE_HEADER:
+                print(f'[!] {raw_image_path.name} is an Android sparse image.')
+            else:
+                break
+        else:
+            print('[!] Wrong file location.')
+
+    if raw_image_path == sparse_image_path:
+        print(
+            '[!] It seems, you overwritten the sparse image.',
+            "Please, DON'T do it again.",
+        )
+        return raw_image_path, ask_sparse_image(raw_image_path)
+
+    return raw_image_path, sparse_image_path
+
+
+def get_partition_images(image_path: Path) -> (Path, Path):
+    """For lpmake we need to work with both sparse and raw image at once.
+
+    Args:
+        image_path: Image Path (can be a sparse or raw one)
+
+    Returns:
+        (Path, Path): The raw image's Path and the sparse image's Path
+    """
+    file_header = tuple(image_path.read_bytes()[:4])
+    if file_header == SPARSE_IMAGE_HEADER:
+        return ask_raw_image(image_path)
+
+    return ask_sparse_image(image_path)
 
 
 def main():  # pylint: disable=too-many-branches,too-many-statements
@@ -32,7 +121,7 @@ def main():  # pylint: disable=too-many-branches,too-many-statements
         sys.exit(1)
 
     print('[...] Trying to find images...')
-    images = tuple(image_dir.glob('*.[iI][mM][gG]'))
+    images = list(image_dir.glob('*.[iI][mM][gG]'))
     if not images:
         print('No images were found.')
         sys.exit(1)
@@ -47,6 +136,7 @@ def main():  # pylint: disable=too-many-branches,too-many-statements
         mode = 'ask'
 
     partitions = []
+    raw_images_paths = []
     images_size = 0
     block_size = 4096
     is_ab = False
@@ -66,12 +156,21 @@ def main():  # pylint: disable=too-many-branches,too-many-statements
                 print('[!] Note: This will be a RW partition.')
                 image_mode = 'none'
 
-        images_size += image_path.stat().st_size
+        print(
+            '[!] Note: Keep the sparse image name clean',
+            "(e.g. 'system_a.img').",
+        )
+        # Note: Unlike raw_images_paths, images can contain
+        # either sparse or raw images.
+        raw_image_path, image_path = get_partition_images(image_path)
+        raw_images_paths.append(raw_image_path)
+
+        images_size += raw_image_path.stat().st_size
         partitions.append(
             '--partition {name}:{ro}:{size}:main --image {name}={path}'.format(
                 name=image_path.stem,
                 ro=image_mode if mode == 'ask' else mode,
-                size=image_path.stat().st_size,
+                size=raw_image_path.stat().st_size,
                 path=str(image_path),
             ),
         )
@@ -91,32 +190,31 @@ def main():  # pylint: disable=too-many-branches,too-many-statements
         print('[!] Note: lpmake will output an Android sparse image.')
         is_sparse = '--sparse'
     else:
-        print('[!] Note: lpmake will NOT output an Android sprarse image.')
+        print('[!] Note: lpmake will NOT output an Android sparse image.')
         is_sparse = ''
+
+    if images_size > super_size:
+        print('[!] Your images are too big to fit in the super partition.')
+        sys.exit(1)
+    elif super_size - images_size < 104857600:  # 104857600 bytes = 100 mb
+        print(
+            '[!] Note: Free super space is less than 100mb.',
+            'This may cause device-specific issues while booting Android.',
+        )
 
     if images_size % block_size != 0:
         print('[!] Output super filesize is not correct. Checking images...')
-        for image_path in images:
+        for image_path in raw_images_paths:
             incomplete_block = image_path.stat().st_size % block_size
 
             if incomplete_block == 0:
                 print(f'[!] Note: {image_path.name} is OK.')
-                break
-
-            file_header = tuple(image_path.read_bytes()[:4])
-            if file_header == (0x3A, 0xFF, 0x26, 0xED):
-                # This is an Android sparse image
-                print(
-                    f'[!] Note: We are unable to check {image_path.name}',
-                    'blocks, due to the usage of sparse images.',
-                    "But don't use the raw ones and IGNORE this notice.",
-                )
             else:
                 print(
                     f'[!] {image_path.name} has a problem',
                     f'(Expected {block_size - incomplete_block} more bytes).',
                 )
-                sys.exit(1)
+        sys.exit(1)
 
     print('[!] Note: metadata-size set to 65536.')
     print('[!] Note: super-name set to super.')
